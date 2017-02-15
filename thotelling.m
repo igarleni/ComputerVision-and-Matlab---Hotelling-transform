63bed20d73db28396ce606536c1b20660cb1f15a
Ítalo Garleni Rodríguez <italogar6@hotmail.com>
2017-02-16 00:51:22 +0100

    Trying some stuff

--- thotelling.m
+++ thotelling.m
@@ -1,15 +1,17 @@
-%Read images
-for i = 1:6
-    path = strcat('adra/banda', int2str(i), '.tif');
+%Read adra images
+for n = 1:6
+    path = strcat('adra/banda', int2str(n), '.tif');
     adraImg = imread(path);
     adraImg = im2double(adraImg);
-    adraImages(:,:,i) = adraImg;
+    adraImages(:,:,n) = adraImg;
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Create Hotelling transform
 
-%Calculate Covariance Matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Applu Hotelling transform
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Calculate Covariance Matrix of pixel vectors
 sizeVector = size(adraImages);
 for i = 1:sizeVector(1)
     for j = 1:sizeVector(2)
@@ -18,3 +20,21 @@ for i = 1:sizeVector(1)
         covMatrix(i,j) = cov(vector);
     end
 end
+
+%Get Eigenvalues and Eigenvectors
+[eigenVectors, eigenValues] = eig(covMatrix);
+
+%Get A = covariance matrix ordered by eigenvalues (max to min)
+A = eigenVectors;
+
+%Apply Hotelling on pixel vectors y = A(x-m)
+for n = 1:6
+    for i = 1:sizeVector(1)
+        for j = 1:sizeVector(2)
+            vector = adraImages(i,j,:);
+            vector = squeeze(vector(1,1,:)).';
+            hotellingImage(i,j,:) = A * (vector - adraImages(i,j,n) );
+        end
+    end
+end
+
